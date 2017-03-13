6b2a48a1fbcaa2264fb55343b4a4217b2c965f79
Joon Park <parkjoon@umich.edu>
2017-03-13 19:10:02 -0400

    Fixed phone numbers not going through.

--- public/js/views/view.contact.js
+++ public/js/views/view.contact.js
@@ -28,59 +28,57 @@ Theme Version:	5.6.0
 				url: $form.attr('action'),
 				data: {
 					name: $form.find('#name').val(),
+					phone: $form.find('#phone').val(),
 					email: $form.find('#email').val(),
-					subject: $form.find('#subject').val(),
 					message: $form.find('#message').val()
 				}
 			}).always(function(data, textStatus, jqXHR) {
-
-				$errorMessage.empty().hide();
-
-				if (data.response == 'success') {
-
-					$messageSuccess.removeClass('hidden');
-					$messageError.addClass('hidden');
-
-					// Reset Form
-					$form.find('.form-control')
-						.val('')
-						.blur()
-						.parent()
-						.removeClass('has-success')
-						.removeClass('has-error')
-						.find('label.error')
-						.remove();
-
-					if (($messageSuccess.offset().top - 80) < $(window).scrollTop()) {
-						$('html, body').animate({
-							scrollTop: $messageSuccess.offset().top - 80
-						}, 300);
-					}
-
-					$submitButton.button('reset');
-					
-					return;
-
-				} else if (data.response == 'error' && typeof data.errorMessage !== 'undefined') {
-					$errorMessage.html(data.errorMessage).show();
-				} else {
-					$errorMessage.html(data.responseText).show();
-				}
-
-				$messageError.removeClass('hidden');
-				$messageSuccess.addClass('hidden');
-
-				if (($messageError.offset().top - 80) < $(window).scrollTop()) {
-					$('html, body').animate({
-						scrollTop: $messageError.offset().top - 80
-					}, 300);
-				}
-
-				$form.find('.has-success')
-					.removeClass('has-success');
-					
-				$submitButton.button('reset');
-
+				// $errorMessage.empty().hide();
+				//
+				// if (data.response == 'success') {
+				//
+				// 	$messageSuccess.removeClass('hidden');
+				// 	$messageError.addClass('hidden');
+				//
+				// 	// Reset Form
+				// 	$form.find('.form-control')
+				// 		.val('')
+				// 		.blur()
+				// 		.parent()
+				// 		.removeClass('has-success')
+				// 		.removeClass('has-error')
+				// 		.find('label.error')
+				// 		.remove();
+				//
+				// 	if (($messageSuccess.offset().top - 80) < $(window).scrollTop()) {
+				// 		$('html, body').animate({
+				// 			scrollTop: $messageSuccess.offset().top - 80
+				// 		}, 300);
+				// 	}
+				//
+				// 	$submitButton.button('reset');
+				//
+				// 	return;
+				//
+				// } else if (data.response == 'error' && typeof data.errorMessage !== 'undefined') {
+				// 	$errorMessage.html(data.errorMessage).show();
+				// } else {
+				// 	$errorMessage.html(data.responseText).show();
+				// }
+				//
+				// $messageError.removeClass('hidden');
+				// $messageSuccess.addClass('hidden');
+				//
+				// if (($messageError.offset().top - 80) < $(window).scrollTop()) {
+				// 	$('html, body').animate({
+				// 		scrollTop: $messageError.offset().top - 80
+				// 	}, 300);
+				// }
+				//
+				// $form.find('.has-success')
+				// 	.removeClass('has-success');
+				//
+				// $submitButton.button('reset');
 			});
 		}
 	});
